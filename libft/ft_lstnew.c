3b93aafa9fee56b65e96da9e053dfd1ade655968
Romain LEMARCHAND <rlemarch@e2r12p7.42.fr>
2015-12-13 12:26:13 +0100

    Save

---
+++ libft/ft_lstnew.c
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstnew.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rlemarch <rlemarch@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2015/12/13 10:47:15 by rlemarch          #+#    #+#             */
+/*   Updated: 2015/12/13 12:18:51 by rlemarch         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+t_list	*ft_lstnew(void const *content, size_t content_size)
+{
+	t_list	*new;
+
+	new = (t_list*)malloc(sizeof(t_list));
+	if (new == NULL)
+		return (NULL);
+	if (content == NULL)
+	{
+		new->content = NULL;
+		new->content_size = 0;
+	}
+	else
+	{
+		new->content = (void*)malloc(content_size * sizeof(void));
+		new->content = ft_memcpy(new->content, content, content_size);
+		new->content_size = content_size;
+		new->next = NULL;
+	}
+	return (new);
+}
